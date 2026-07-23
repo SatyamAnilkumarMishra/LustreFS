1c7ac7817dbe5e74b030ccc7c9ec5ea58e793598
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2026-07-23 21:58:35 +0530

    Update manager.hpp

--- include/manager.hpp
+++ include/manager.hpp
@@ -1,14 +1,14 @@
-#ifndef LUSTRE_MANAGER_HPP
-#define LUSTRE_MANAGER_HPP
+#ifndef MANAGER_HPP
+#define MANAGER_HPP
 
-#include <string>
+class SystemManager {
+public:
+    static void run_full_pipeline();
+};
 
 class LustreManager {
 public:
-    static bool format_mgt_mdt(const std::string& dev, const std::string& fsname);
-    static bool format_ost(const std::string& dev, const std::string& fsname, const std::string& mgs_node, int ost_index);
-    static bool mount_target(const std::string& dev, const std::string& mount_point);
-    static bool mount_client(const std::string& mgs_spec, const std::string& client_mount);
+    static void run_full_pipeline();
 };
 
-#endif
+#endif 

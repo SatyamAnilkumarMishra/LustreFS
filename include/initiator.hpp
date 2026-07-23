bf972e8d57a5d66968826a48febb1718f830a937
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2026-07-23 22:09:46 +0530

    Add discover_and_login method to ISCSIInitiator

--- include/initiator.hpp
+++ include/initiator.hpp
@@ -5,6 +5,7 @@
 
 class ISCSIInitiator {
 public:
+    static std::string discover_and_login(const std::string& target_ip, const std::string& target_iqn);
     static bool connect_target(const std::string& target_ip, const std::string& target_iqn);
 };
 

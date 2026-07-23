95ccf4c7e63f188d518be0b335b658cbfacd768d
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2026-07-23 21:59:28 +0530

    Rename header guards in target.hpp

--- include/target.hpp
+++ include/target.hpp
@@ -1,15 +1,11 @@
-#ifndef ISCSI_TARGET_HPP
-#define ISCSI_TARGET_HPP
+#ifndef TARGET_HPP
+#define TARGET_HPP
 
 #include <string>
 
 class ISCSITarget {
 public:
-    static bool setup_export(const std::string& backstore_name, 
-                             const std::string& img_path, 
-                             const std::string& size_gb, 
-                             const std::string& target_iqn);
-    static bool teardown_export(const std::string& backstore_name, const std::string& target_iqn);
+    static bool setup_target(const std::string& img_path, const std::string& target_iqn);
 };
 
-#endif
+#endif 

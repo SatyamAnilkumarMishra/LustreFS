20979f5bdce303ec144d025481e7aafe720459ea
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2026-07-23 12:23:07 +0530

    Rename header guard in injector.hpp

--- include/injector.hpp
+++ include/injector.hpp
@@ -1,13 +1,11 @@
-#ifndef FAULT_INJECTOR_HPP
-#define FAULT_INJECTOR_HPP
+#ifndef INJECTOR_HPP
+#define INJECTOR_HPP
 
 #include <string>
 
 class FaultInjector {
 public:
     static bool inject_iscsi_session_drop(const std::string& target_iqn);
-    static bool inject_network_drop(const std::string& interface_name, int duration_sec);
-    static bool simulate_target_crash();
 };
 
-#endif
+#endif 

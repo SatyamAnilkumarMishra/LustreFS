92f3ad6406e193e14c51a062962229b8ccc6d8a8
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2026-07-23 22:09:11 +0530

    Refactor SystemManager and LustreManager classes

--- include/manager.hpp
+++ include/manager.hpp
@@ -1,14 +1,18 @@
 #ifndef MANAGER_HPP
 #define MANAGER_HPP
 
+#include <string>
+
+class LustreManager {
+public:
+    static bool format_mgt_mdt(const std::string& dev, const std::string& fs_name);
+    static bool mount_target(const std::string& dev, const std::string& mount_point);
+    static void run_full_pipeline();
+};
+
 class SystemManager {
 public:
     static void run_full_pipeline();
 };
 
-class LustreManager {
-public:
-    static void run_full_pipeline();
-};
-
-#endif 
+#endif

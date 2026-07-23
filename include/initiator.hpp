4778a13e17a99c5dbe9232cb43fda3e85837b7a4
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2026-07-23 22:00:57 +0530

    Rename header guards in initiator.hpp

--- include/initiator.hpp
+++ include/initiator.hpp
@@ -1,12 +1,11 @@
-#ifndef ISCSI_INITIATOR_HPP
-#define ISCSI_INITIATOR_HPP
+#ifndef INITIATOR_HPP
+#define INITIATOR_HPP
 
 #include <string>
 
 class ISCSIInitiator {
 public:
-    static std::string discover_and_login(const std::string& target_ip, const std::string& target_iqn);
-    static bool logout(const std::string& target_iqn);
+    static bool connect_target(const std::string& target_ip, const std::string& target_iqn);
 };
 
-#endif
+#endif 
